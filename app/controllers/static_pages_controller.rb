3fb6e7fc52d6b68400d1c1c66e339966102c79eb
App Academy Student <student@appacademy.io>
2015-12-09 11:16:52 -0800

    First commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def content
+  end
+end
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/OrganGrinder.jsx",
+  output: {
+    path: "./",
+    filename: "./app/assets/javascripts/bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", '.jsx']
+  }
+};
